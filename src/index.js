f393661ba0d0aff113c9598ce0c1bd0a7a1bbef4
Filipe <filipess671@gmail.com>
2023-06-08 17:01:57 -0300

    Chore: add bootstrap

--- src/index.js
+++ src/index.js
@@ -9,12 +9,8 @@ app.engine('handlebars', exphbs.engine())
   .set('view engine', 'handlebars')
   .set('views', __dirname + '/views')
   .use('/assets', express.static(__dirname + '/assets'))
-  .use('/bootstrap/dist/css', express.static('/node_modules/bootstrap/dist/css'))
+  .use('/bootstrap', express.static(__dirname, '..', 'node_modules/bootstrap/'))
   .use('/', routes);
-
-app.get('/', () => {
-  console.log(__dirname);
-});
 
 app.listen(port, () => {
   console.log(`Servidor iniciado em http://localhost:${port}`);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+routes.get('/', (req, res) => {
+  res.render('index', {});
+});
 
 module.exports =  routes;
