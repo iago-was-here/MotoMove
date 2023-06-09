0bd76be187356234932dfb6a2d2aaeb52b2614a3
Caroline Melo <84986038+CarolineFMelo@users.noreply.github.com>
2023-06-09 16:05:12 -0300

    tela inicial pronta

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,10 +9,33 @@ routes.get('/',function(req, res){
 // Tela Inicial sem login
 routes.get('/inicio', function(req, res) {
   const dadosTemplate = {
-    title: 'MOTO MOVE | Bem-Vindo',
-    style: 'public/css/style.css'
+    title: 'MOTO MOVE | Bem-Vindo'
   };
   res.render('inicio', dadosTemplate);
 });
 
-module.exports =  routes;
+// Login
+routes.get('/login', function(req, res) {
+  const dadosTemplate = {
+    title: 'MOTO MOVE | Login' 
+  };
+  res.render('login', dadosTemplate);
+});
+
+// Cadastro Cliente
+routes.get('/cadastro/cliente', function(req, res) {
+  const dadosTemplate = {
+    title: 'MOTO MOVE | Cadastro Cliente'
+  };
+  res.render('cadastro-cliente', dadosTemplate);
+});
+
+// Cadastro Motorista
+routes.get('/cadastro/motorista', function(req, res) {
+  const dadosTemplate = {
+    title: 'MOTO MOVE | Cadastro Motorista'
+  };
+  res.render('cadastro-motorista', dadosTemplate);
+});
+
+module.exports = routes;
